c58a988f0ec7bed295cf9213c3c94a745b0ca8e1
Xavi Ivars <xavi.ivars@gmail.com>
2016-10-18 00:01:47 +0200

    Change pool logic

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function(err, req, res, next) {
 });
 
 var pool = mysql.createPool({
-    connectionLimit: 10, //important
+    connectionLimit: 20, //important
     host: process.env.dbhost,
     user: process.env.dbuser,
     password: process.env.dbpass,
@@ -64,7 +64,7 @@ var getLog = function(log, cookies) {
         $log.correctorUuid = cookies.correctorUuid;
     }
 
-    return [$log.type, $log.rule_id, $log.rule, $log.incorrect_text, $log.incorrect_position, $log.context, $log.suggestion, $log.suggestion_position, $log.correctorUuid];
+    return [$log.type, $log.rule_id, $log.rule_sub_id, $log.incorrect_text, $log.incorrect_position, $log.context, $log.suggestion, $log.suggestion_position, $log.correctorUuid];
 }
 
 app.get('/format/', function (req, res) {
@@ -81,7 +81,7 @@ app.get('/stats/', function (req, res) {
         var $query = 'select count(*) as total from lt_stats';
 
         connection.query($query, function (err, rows) {
-
+            connection.release();
             if (!err) {
                 res.json(Object.assign({}, {"code": 200}, rows[0]));
             } else {
@@ -93,17 +93,17 @@ app.get('/stats/', function (req, res) {
 
 app.post("/log/", function (req, res) {
 
-    pool.getConnection(function (err, connection) {
-        if (err) {
-            res.json({"code": 100, "status": "Error in connection database"});
-            return;
-        }
+    var $log = getLog(req.body, req.cookies, res);
 
-        var $log = getLog(req.body, req.cookies, res);
+    if (!$log) {
+        res.json({"code": 200, 'status': 'Not yet ready'});
+    } else {
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                res.json({"code": 100, "status": "Error in connection database"});
+                return;
+            }
 
-        if (!$log) {
-            res.json({"code": 200, 'status': 'Not yet ready'});
-        } else {
             res.cookie('correctorUuid', $log[8]);
 
             var $query = 'insert into lt_stats(type, rule_id, rule_sub_id, incorrect_text, incorrect_position, context, suggestion, suggestion_position, user_uuid) values(?,?,?,?,?,?,?,?,?)';
@@ -116,8 +116,8 @@ app.post("/log/", function (req, res) {
                     res.json({"code": 500, "status": "Error in database"});
                 }
             });
-        }
-    });
+        });
+    }
 });
 
 app.listen(3000);
